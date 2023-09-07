3f64695630e039fdc4f49e383948937aac47e347
ANNALIISA_LUIK <alluik@ymail.com>
2023-09-07 15:57:05 +0300

    ANNA-LIISA putting only 1 div in signupview template

--- src/views/SignUpView.vue
+++ src/views/SignUpView.vue
@@ -1,43 +1,53 @@
 <template>
 
   <div class="container text-center">
-    <div class="row align-items-start">
-      <div class="col">
-        <div class="row justify-content-center">
-          <div class="col col-6 form-floating mb-3">
-            <input type="email" class="form-control" placeholder="name@example.com">
-            <label for="floatingInput" class="custom-label">Eesnimi</label>
+    <div class="row">
+      <div class="col col-4">
+        <div class="row align-items-start">
+
+          <div class="row justify-content-center">
+            <div class="col col-5 form-floating mb-3">
+              <input type="email" class="form-control" placeholder="name@example.com">
+              <label for="floatingInput" class="custom-label">Eesnimi</label>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col col-5 form-floating mb-3">
+              <input type="email" class="form-control" placeholder="name@example.com">
+              <label for="floatingInput" class="custom-label">Kasutajanimi</label>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col col-5 form-floating mb-3">
+              <input type="email" class="form-control" placeholder="name@example.com">
+              <label for="floatingInput" class="custom-label">Parool</label>
+            </div>
+          </div>
+          <div class="row justify-content-center">
+            <div class="col col-5 form-floating mb-3">
+              <input type="email" class="form-control" placeholder="name@example.com">
+              <label for="floatingInput" class="custom-label">Korda parooli</label>
+            </div>
           </div>
         </div>
-        <div class="row justify-content-center">
-          <div class="col col-6 form-floating mb-3">
-            <input type="email" class="form-control" placeholder="name@example.com">
-            <label for="floatingInput" class="custom-label">Kasutajanimi</label>
-          </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col col-6 form-floating mb-3">
-            <input type="email" class="form-control" placeholder="name@example.com">
-            <label for="floatingInput" class="custom-label">Parool</label>
-          </div>
-        </div>
-        <div class="row justify-content-center">
-          <div class="col col-6 form-floating mb-3">
-            <input type="email" class="form-control" placeholder="name@example.com">
-            <label for="floatingInput" class="custom-label">Korda parooli</label>
-          </div>
-        </div>
-        One of three columns
       </div>
-      <div class="col">
+
+      <div class="col col-4">
+        empty col
+      </div>
+
+      <div class="col col-4">
+
+
         <div class="row justify-content-center">
-          <div class="col col-6 form-floating mb-3">
+          <div class="col col-5 form-floating mb-3">
             <input type="email" class="form-control" placeholder="name@example.com">
             <label for="floatingInput" class="custom-label">Perekonnanimi</label>
           </div>
         </div>
         <div class="dropdown">
-          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown"
+                  aria-expanded="false">
             Vali pakett
           </button>
           <ul class="dropdown-menu">
@@ -46,13 +56,37 @@
             <li><a class="dropdown-item" href="#">Backend</a></li>
           </ul>
         </div>
-        One of three columns
+
       </div>
+
+
+    </div>
+    <div class="row">
+      <div class="col col-5">
+
+      </div>
+      <div class="col col-5">
+        <div class="row align-items-center">
+          <div class="col col-6 align-items-center">
+            <div class="form-check">
+              <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+              <label class="form-check-label" for="flexCheckDefault">
+                Default checkbox
+              </label>
+            </div>
+          </div>
+        </div>
+        <div class="row align-items-center">
+          <div class="col col-6 align-items-center">
+            <a class="btn btn-primary" href="#" role="button">Registeeru</a>
+          </div>
+        </div>
+      </div>
+      <div class="col col-5"></div>
     </div>
   </div>
-  <div>
-    <a class="btn btn-primary" href="#" role="button">Registeeru</a>
-  </div>
+
+
 </template>
 
 
@@ -68,6 +102,9 @@ export default {
   font-size: 16px;
   font-weight: bold;
   color: rgb(31, 32, 35);
+}
+.row > .col-4 {
+  margin: 0.05rem;
 }
 
 </style>
