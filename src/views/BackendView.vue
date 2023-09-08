8acad1c4392b051038da18b79f276a94812aabe7
marinurme <marinurme98@gmail.com>
2023-09-08 13:54:22 +0300

    MARI added Frontend and Backend View

--- src/views/BackendView.vue
+++ src/views/BackendView.vue
@@ -1,12 +1,25 @@
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col">
+        Column
+      </div>
+      <div class="col">
+        Column
+      </div>
+      <div class="col">
+        Column
+      </div>
+    </div>
+  </div>
+</template>
+
 <script>
 export default {
   name: "BackendView"
+
 }
 </script>
-
-<template>
-
-</template>
 
 <style scoped>
 
