121a4ad0231f3e9954c784af943e04df08a9203c
ANNALIISA_LUIK <alluik@ymail.com>
2023-09-08 14:15:30 +0300

    ANNA-LIISA signupview post meetod tehtud

--- src/assets/script/AlertMessage.js
+++ src/assets/script/AlertMessage.js
@@ -1,1 +1,3 @@
 export const FILL_MANDATORY_FIELDS = 'Täida kõik väljad'
+export const PASSWORD_MISMATCH = 'Paroolid peavad kattuma'
+export const USER_ADDED = 'Kasutajaks registreeritud'
--- src/assets/script/ErrorCode.js
+++ src/assets/script/ErrorCode.js
@@ -1,1 +1,2 @@
 export const INCORRECT_CREDENTIALS = 111
+export const USERNAME_UNAVAILABLE = 333
