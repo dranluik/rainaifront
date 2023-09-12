14225b024f7cbe531c868772a60ae8961b44728e
marinurme <marinurme98@gmail.com>
2023-09-12 13:48:26 +0300

    MARI fixed lessonstable in backendView

--- src/views/BackendView.vue
+++ src/views/BackendView.vue
@@ -2,19 +2,18 @@
   <div class="container text-center justify-content-center">
     <div class="row justify-content-center">
       <div class="col col-3">
-        <TechnologiesDropdown :packageTypeId="packageTypeId" @event-update-selected-technology-package-type-id="updateSelectedTechnologyPackageTypeId"/>
+        <TechnologiesDropdown :packageTypeId="packageTypeId" @event-update-selected-technology-id="updateSelectedTechnologyId"/>
 
       </div>
       <div class="col col-3 d-flex justify-content-center">
-        <LessonsTable :packageTypeId="packageTypeId" :selectedTechnologyId="selectedTechnologyPackageTypeId"
-                      @event-update-selected-technology-id="updateSelectedTechnologyPackageTypeId"/>
+        <LessonsTable :packageTypeId="packageTypeId" :selectedTechnologyId="selectedTechnologyId"
+                      @event-update-selected-technology-id="updateSelectedTechnologyId"/>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import router from "@/router";
 import TechnologiesDropdown from "@/components/dropdown/TechnologiesDropdown.vue";
 import LessonsTable from "@/components/table/LessonsTable.vue";
 
@@ -24,36 +23,15 @@ export default {
   components: {LessonsTable, TechnologiesDropdown,},
   data(){
     return {
-      lessonsNames: [
-        {
-          lessonName: String,
-          isSelected: true
-        }
-      ],
-      selectedTechnologyPackageTypeId: 0,
+      selectedTechnologyId: 0,
       packageTypeId: 3
 
     }
   },
   methods: {
 
-    updateSelectedTechnologyPackageTypeId(selectedPackageTypeId){
-      this.selectedTechnologyPackageTypeId = selectedPackageTypeId
-    },
-
-    getLessonNames() {
-      this.$http.get("/lesson/user", {
-            params: {
-              userId: this.userId,
-              packageTypeId: this.packageTypeId,
-              technologyId: this.technologyId,
-            }
-          }
-      ).then(response => {
-        this.lessonsNames = response.data
-      }).catch(error => {
-        router.push({name:'errorRoute'})
-      })
+    updateSelectedTechnologyId(selectedTechnologyId){
+      this.selectedTechnologyId = selectedTechnologyId
     },
   },
 }
