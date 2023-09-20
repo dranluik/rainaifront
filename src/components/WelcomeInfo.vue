192a10754d2bd22fa32dabf9f156efb21d7ba689
Martin <martin.keilakk@gmail.com>
2023-09-20 15:53:57 +0300

    MARTIN Editor page changed

--- src/components/WelcomeInfo.vue
+++ src/components/WelcomeInfo.vue
@@ -12,16 +12,20 @@
         </h1>
         <p class="aligned-paragraph">
           Meie kursused on mõeldud nii algajatele kui ka juba kogenud arendajatele, kes soovivad oma oskusi täiendada.
-          Olgu teie eesmärk karjääri alustamine <br>veebiarenduses või oma projektide loomine,
+          Olgu teie eesmärk karjääri alustamine veebiarenduses või oma projektide loomine,<br>
           meie kursused aitavad teil saavutada oma eesmärke.
           <br>
-          Liitu [Ettevõtte nimi] kursustega ja avasta veebiarenduse maailm oma käe järgi.<br>
+          Liitu <b>RainAI</b> kursustega ja avasta veebiarenduse maailm oma käe järgi.<br>
           Oleme siin, et aidata teid oma teekonnal saada edukaks veebiarendajaks.<br>
         </p>
       </div>
 
       <div class="col col-3 text-center">
-        empty colllll
+        Kuidas meiega ühendust võtta?
+
+        Külastage meie veebisaiti aadressil [veebiaadress] või võtke meiega ühendust e-posti teel aadressil [e-posti aadress]. Oleme alati valmis vastama teie küsimustele ja aitama teil leida õige õppekava.
+
+        Liitu [Ettevõtte nimi]-ga täna ja astu esimene samm oma põneva veebiarenduse karjääri suunas!
       </div>
     </div>
 </template>
