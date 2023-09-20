771d59338eae7c2feae25485588cb5d9cdf3e00a
marinurme <marinurme98@gmail.com>
2023-09-20 23:50:57 +0300

    MARI fixed gif in navbar

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <LogOutModal @event-update-nav-menu="updateNavMenu" ref="logOutModalRef"/>
-  <div class="nav-container">
-    <img src="@/assets/RainAiUus.gif" height="100" width="100" alt="" class="left-aligned"/>
-  <nav>
-    <router-link to="/">Kodu</router-link> |
-    <template v-if="isLoggedIn">
-    <router-link :to="{name: 'frontendRoute'}">Frontend</router-link> |
-    <router-link :to="{name: 'backendRoute'}">Backend</router-link> |
-    <router-link :to="{name: 'myLessonsRoute'}">Minu teemad</router-link> |
-      <router-link v-if="this.$route.path === '/editor'" :to="{name: 'editorRoute'}">Toimetaja</router-link> |
-    <router-link to="#" @click="handleLogout">Logi välja</router-link>
-    </template>
-    <template v-if="!isLoggedIn">
-    <router-link :to="{name: 'signupRoute'}">Registreeru kasutajaks</router-link> |
-    <router-link :to="{name: 'loginRoute'}">Logi Sisse</router-link>
-    </template>
-  </nav>
-  </div>
-  <router-view @event-update-nav-menu="updateNavMenu" />
+    <nav class="nav nav-pills nav-fill">
+      <a class="navbar-brand mr-auto" style="padding-left:50px" href="#">
+        <img src="@/assets/RainAiUus.gif" width="100" height="100"  alt=""/>
+      </a>
+      <ul class="nav-links">
+          <router-link class="nav-item active" to="/">Kodu</router-link>
+          |
+        <template v-if="isLoggedIn">
+          <router-link class="nav-item" :to="{name: 'frontendRoute'}">Frontend</router-link>
+          |
+          <router-link class="nav-item" :to="{name: 'backendRoute'}">Backend</router-link>
+          |
+          <router-link class="nav-item" :to="{name: 'myLessonsRoute'}">Minu teemad</router-link>
+          |
+          <router-link class="nav-item" v-if="this.$route.path === '/editor'" :to="{name: 'editorRoute'}">Toimetaja</router-link>
+
+          <router-link class="nav-item" to="#" @click="handleLogout">Logi välja</router-link>
+        </template>
+        <template v-if="!isLoggedIn">
+          <router-link class="nav-item" :to="{name: 'signupRoute'}">Registreeru kasutajaks</router-link>
+          |
+          <router-link class="nav-item" :to="{name: 'loginRoute'}">Logi Sisse</router-link>
+        </template>
+      </ul>
+    </nav>
+  <router-view @event-update-nav-menu="updateNavMenu"/>
 </template>
 
 <script>
@@ -67,18 +75,18 @@ html, body {
   flex-direction: column;
   min-height: 100vh;
 
-  align-items: center;/* Ensure the content covers the entire viewport height */
+  align-items: center; /* Ensure the content covers the entire viewport height */
 }
 
 nav {
-  width: 100%;
-  padding: 30px;
+   width: 100%;
+  /* padding-left: 30px;
+  /* align-items: center;*/
   background-color: black;
-  align-items: center;
 }
 
 nav a {
-  margin-left: 10px;
+  /* margin-left: 10px;*/
   font-weight: bold;
   color: #ebebef;
 }
@@ -87,16 +95,9 @@ nav a.router-link-exact-active {
   color: #46fff2;
 }
 
-.nav-container {
-  display: flex;
-  align-items: center;
-  background-color: black; /* Set the background color of the container */
-  padding: 0px; /* Add padding for spacing */
+.nav-links {
+  margin-left: 25%;
+  margin-top: 2.5%;
 }
 
-.left-aligned {
-  display: flex;
-  /* Use CSS styles to align the image to the left */ /* Float the image to the left */
-  margin-right: 0px; /* Add some margin to the right (adjust as needed) */
-}
 </style>
