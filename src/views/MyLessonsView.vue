202c890e39c9dd50360e5a88ac654ef39866d72a
marinurme <marinurme98@gmail.com>
2023-09-21 10:43:44 +0300

    MARI disain

--- src/views/MyLessonsView.vue
+++ src/views/MyLessonsView.vue
@@ -12,9 +12,9 @@
           <MyLessonsTable :lessonId="lessonId" :lessonName="lessonName" @event-lesson-change="updateLessonId"/>
         </div>
       </div>
-      <div  v-if="lessonContentResponse.editorContent !== ''" class="card bg-light" style="max-width: 35rem;">
+      <div v-if="lessonContentResponse.editorContent !== ''" class="card bg-light ms-5" style="max-width: 50rem;">
         <div class="card-header">{{lessonName}}</div>
-        <div class="card-body">
+        <div class="card-body text-dark">
           <p class="card-text lesson-content" v-html="lessonContentResponse.editorContent"></p>
         </div>
       <div class="media-container" v-if="images.length > 0">
