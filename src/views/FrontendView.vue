61fdbaf53bf4a06e7a1378333fb6563e4528fa86
Martin <martin.keilakk@gmail.com>
2023-09-21 10:13:58 +0300

    MARTIN - Front Back background changed

--- src/views/FrontendView.vue
+++ src/views/FrontendView.vue
@@ -1,4 +1,5 @@
 <template>
+  <div class="background-container">
   <div class="container text-center justify-content-center">
     <div class="row justify-content-center">
       <div class="col col-3">
@@ -10,6 +11,7 @@
                      @event-update-selected-technology-id="updateSelectedTechnologyId"/>
       </div>
     </div>
+  </div>
   </div>
 </template>
 <script>
@@ -40,3 +42,17 @@ export default {
   }
 }
 </script>
+<style scoped>
+.background-container {
+  /* Set the background image using URL */
+  background-image: url("../assets/StudyBackground.png"); /* Adjust the path to your image */
+  /* Set the background size and other properties as needed */
+  background-size: cover; /* or 'contain' or custom values */
+  background-repeat: no-repeat;
+  background-attachment: fixed; /* Optional: To make the background fixed */
+  /* Add more styles for your container */
+  width: 100%;
+  height: 86vh; /* Make it cover the entire viewport */
+  /* Add other styles as needed */
+}
+</style>
