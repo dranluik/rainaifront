bd43cdadd69f274f6d3677da36ebc6ef44e8031c
ANNALIISA_LUIK <alluik@ymail.com>
2023-09-21 12:46:43 +0300

    ANNA-LIISA deletelesson minu teemades

--- src/components/table/MyLessonsTable.vue
+++ src/components/table/MyLessonsTable.vue
@@ -14,7 +14,7 @@
           :class="{'selected-lesson': userLesson.lessonId === selectedLessonId}"
           class="hoverable-link">{{ userLesson.lessonName }}</td>
       <td @click="navigateToEditor(userLesson.lessonId)" v-if="roleName === 'admin'"><font-awesome-icon :icon="['far', 'pen-to-square']" size="lg" class="hoverable-link m-2"/></td>
-      <td v-if="roleName === 'admin'"><font-awesome-icon :icon="['fas', 'trash']" size="lg" class="hoverable-link m-2"/></td>
+      <td @click="deleteLesson(userLesson.lessonId)" v-if="roleName === 'admin'"><font-awesome-icon :icon="['fas', 'trash']" size="lg" class="hoverable-link m-2"/></td>
     </tr>
     </tbody>
   </table>
@@ -70,7 +70,20 @@ export default {
       this.selectedLessonId = lessonId
       this.selectedLessonName = lessonName
       this.$emit('event-lesson-change', lessonId, lessonName)
-    }
+    },
+    deleteLesson(lessonId) {
+      this.$http.delete("/myLessons", {
+            params: {
+              lessonId: lessonId,
+
+            }
+          }
+      ).then(response => {
+        this.getUserLessons()
+      }).catch(error => {
+        router.push({name: 'errorRoute'})
+      })
+    },
 
   },
   beforeMount() {
