ac77cba79518e59c88a072fa2c1e896d18e0aa9e
ANNALIISA_LUIK <alluik@ymail.com>
2023-09-21 14:25:33 +0300

    ANNA-LIISA navigator nupud teised

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,44 @@
 <template>
   <LogOutModal @event-update-nav-menu="updateNavMenu" ref="logOutModalRef"/>
-    <nav class="nav nav-pills nav-fill">
-      <a class="navbar-brand mr-auto" style="padding-left:50px" href="#" @click="openLink">
-        <img src="@/assets/RainAiUus.gif" width="100" height="100"  alt=""/>
-      </a>
-      <ul class="nav-links">
-          <router-link class="nav-item active" to="/">Kodu</router-link>
-          |
-        <template v-if="isLoggedIn">
-          <router-link v-if="isAdmin || isFull || isFrontend" class="nav-item" :to="{name: 'frontendRoute'}">Frontend</router-link>
-          |
-          <router-link v-if="isAdmin || isFull || isBackend" class="nav-item" :to="{name: 'backendRoute'}">Backend</router-link>
-          |
-          <router-link class="nav-item" :to="{name: 'myLessonsRoute'}">Minu teemad</router-link>
-          |
-          <router-link class="nav-item" v-if="this.$route.path === '/editor'" :to="{name: 'editorRoute'}">Toimetaja</router-link>
+  <nav class="nav nav-pills nav-fill">
+    <a class="navbar-brand mr-auto" style="padding-left:50px" href="#" @click="openLink">
+      <img src="@/assets/RainAiUus.gif" width="100" height="100" alt=""/>
+    </a>
+    <ul class="nav-links">
+      <router-link class="nav-item active" to="/"
+                   :class="{ 'active-link': $route.name === 'homeRoute' }">Kodu
+      </router-link>
 
-          <router-link class="nav-item" to="#" @click="handleLogout">Logi välja</router-link>
-        </template>
-        <template v-if="!isLoggedIn">
-          <router-link class="nav-item" :to="{name: 'signupRoute'}">Registreeru kasutajaks</router-link>
-          |
-          <router-link class="nav-item" :to="{name: 'loginRoute'}">Logi Sisse</router-link>
-        </template>
-      </ul>
-    </nav>
+      <template v-if="isLoggedIn">
+        <router-link v-if="isAdmin || isFull || isFrontend" class="nav-item" :to="{name: 'frontendRoute'}"
+                     :class="{ 'active-link': $route.name === 'frontendRoute' }">Frontend
+        </router-link>
+
+        <router-link v-if="isAdmin || isFull || isBackend" class="nav-item" :to="{name: 'backendRoute'}"
+                     :class="{ 'active-link': $route.name === 'backendRoute' }">Backend
+        </router-link>
+
+        <router-link class="nav-item" :to="{name: 'myLessonsRoute'}"
+                     :class="{ 'active-link': $route.name === 'myLessonsRoute' }">Minu teemad
+        </router-link>
+
+        <router-link class="nav-item" v-if="this.$route.path === '/editor'" :to="{name: 'editorRoute'}"
+                     :class="{ 'active-link': $route.name === 'editorRoute' }">Toimetaja
+        </router-link>
+
+        <router-link class="nav-item" to="#" @click="handleLogout">Logi välja</router-link>
+      </template>
+      <template v-if="!isLoggedIn">
+        <router-link class="nav-item" :to="{name: 'signupRoute'}"
+                     :class="{ 'active-link': $route.name === 'signupRoute' }">Registreeru kasutajaks
+        </router-link>
+
+        <router-link class="nav-item" :to="{name: 'loginRoute'}"
+                     :class="{ 'active-link': $route.name === 'loginRoute' }">Logi Sisse
+        </router-link>
+      </template>
+    </ul>
+  </nav>
   <router-view @event-update-nav-menu="updateNavMenu"/>
 </template>
 
@@ -89,7 +103,7 @@ html, body {
 }
 
 nav {
-   width: 100%;
+  width: 100%;
   /* padding-left: 30px;
   /* align-items: center;*/
   background-color: black;
@@ -110,4 +124,26 @@ nav a.router-link-exact-active {
   margin-top: 2.5%;
 }
 
+
+
+</style>
+
+<style scoped>
+.nav-item {
+//border: 2px solid #ffffff; /* Border color */ border-radius: 5px; /* Rounded corners */
+  padding: 10px 20px; /* Padding for text */
+  margin: 10px;
+  color: #ffffff; /* Text color */
+  text-decoration: none; /* Remove underline */
+  background-color: hsla(0, 52%, 44%, 0.5); /* Background color */
+}
+.active-link {
+//border: 2px solid #ffffff; /* Border color */ border-radius: 5px; /* Rounded corners */
+  padding: 10px 20px; /* Padding for text */
+  margin: 10px;
+  color: #ffffff; /* Text color */
+  text-decoration: none; /* Remove underline */
+  background-color: hsla(0, 52%, 44%, 1); /* Background color */
+  font-weight: bold
+}
 </style>
